01cbf08f3ba3ca41835054562eb846dd1fce82a9
Alireza Hajli <hajlialireza@gmail.com>
2023-01-06 20:58:53 +0330

    api urls handled by ninja.url

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
-from .api import api
+from django.urls import path,include
+from socialmedia import api
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", api.urls),  # <---------- !
+    path("api/", api.urls),
 ]
