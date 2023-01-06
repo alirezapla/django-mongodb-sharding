62340935e8824123ee3b747914a42c83fbe171c1
Alireza Hajli <hajlialireza@gmail.com>
2023-01-06 20:56:24 +0330

    mongo commands for initializing

--- scripts/configserver.js
+++ scripts/configserver.js
@@ -6,7 +6,6 @@ rs.initiate(
       members: [
          { _id: 0, host : "config1:27017" },
          { _id: 1, host : "config2:27017" },
-         { _id: 2, host : "config3:27017" }
       ]
    }
 )
