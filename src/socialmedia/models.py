59a4222aa3f5e5ed2d0440957379da06945bea97
Alireza Hajli <hajlialireza@gmail.com>
2023-01-06 21:01:02 +0330

    User,Comment,Like,Post document models

--- src/socialmedia/models.py
+++ src/socialmedia/models.py
@@ -1,29 +1,75 @@
 import datetime
 from django_mongoengine import Document, EmbeddedDocument, fields
-
-
-class Comment(EmbeddedDocument):
-    created_at = fields.DateTimeField(
-        default=datetime.datetime.now, editable=False,
-    )
-    author = fields.StringField(verbose_name="Name", max_length=100)
-    text = fields.StringField(verbose_name="Comment")
-    # replay_to=
-
-
-class Post(Document):
-    created_at = fields.DateTimeField(
-        default=datetime.datetime.now, editable=False,
-    )
-    content = fields.StringField(max_length=255)
-    comments = fields.ListField(
-        fields.EmbeddedDocumentField('Comment'), blank=True,
-    )
+import mongoengine as db
 
 
 class User(Document):
-    created_at = fields.DateTimeField(
+    created_at = db.DateTimeField(
         default=datetime.datetime.now, editable=False,
     )
-    username = fields.StringField(max_length=100)
+    username = db.StringField(max_length=100)
 
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+        }
+
+    def created_datetime(self):
+        return self.created_at
+
+
+class Like(EmbeddedDocument):
+    created_at = db.DateTimeField(
+        default=datetime.datetime.now, editable=False,
+    )
+    liker = db.StringField(max_length=100)
+
+    def as_dict(self):
+        return {
+            'liker': self.liker,
+        }
+
+    def created_datetime(self):
+        return self.created_at
+
+
+class Comment(EmbeddedDocument):
+    created_at = db.DateTimeField(
+        default=datetime.datetime.now, editable=False,
+    )
+    author = db.StringField(max_length=100)
+    text = db.StringField(verbose_name="Comment")
+
+    def as_dict(self):
+        return {
+            'author': self.author,
+            'text': self.text,
+        }
+
+    def created_datetime(self):
+        return self.created_at
+
+
+class Post(Document):
+    created_at = db.DateTimeField(
+        default=datetime.datetime.now, editable=False,
+    )
+    owner = db.ReferenceField(User, max_lenght=100)
+    likes = db.ListField(
+        db.EmbeddedDocumentField('Like'), blank=True,
+    )
+    content = db.StringField(max_length=255)
+    comments = db.ListField(
+        db.EmbeddedDocumentField('Comment'), blank=True,
+    )
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'owner': self.owner,
+            'likes': self.likes,
+            'content': self.content,
+            'comments': self.comments,
+            'created_at': self.created_at
+        }
