f21db85952f7ebc8906192a00f71174073782966
alireza hajli <hajlialireza@gmail.com>
2023-01-28 22:40:24 +0330

    Update api.py

--- src/socialmedia/api.py
+++ src/socialmedia/api.py
@@ -10,27 +10,27 @@ def root(request):
     return {"message": "Welcome to Django with MongoDB"}
 
 
-@api.post("/user", tags=['Users'])
+@api.post("/users", tags=['Users'])
 def new_user(request, user_body: UserSchema):
     res = user.add(user_body)
     return {"response": res}
 
 
-@api.get("/user/{user_id}", tags=['Users'])
+@api.get("/users/{user_id}", tags=['Users'])
 def retrive_user(request, user_id: str):
     validation_object_id(user_id)
     res = user.retrive(user_id)
     return {"response": res}
 
 
-@api.delete("/user/{user_id}", tags=['Users'])
+@api.delete("/users/{user_id}", tags=['Users'])
 def remove_user(request, user_id: str):
     validation_object_id(user_id)
     res = user.remove(user_id)
     return {"response": res}
 
 
-@api.post("/post", tags=['Posts'])
+@api.post("/posts", tags=['Posts'])
 def add_post(request, post_body: PostSchema):
     res = post.add_new(post_body)
     return {"response": res}
@@ -43,7 +43,7 @@ def user_posts(request, user_id: str):
     return {"response": res}
 
 
-@api.post("/like/{post_id}", tags=['Likes'])
+@api.post("/likes/{post_id}", tags=['Likes'])
 def like_it(request, post_id: str, like_body: LikeSchema):
     validation_object_id(post_id)
     res = post.update_by_like(post_id, like_body.dict())
@@ -64,7 +64,7 @@ def comments_of_post(request, post_id: str):
     return {"response": res}
 
 
-@api.post("/comment/{post_id}", tags=['Comments'])
+@api.post("/comments/{post_id}", tags=['Comments'])
 def new_comment(request, post_id: str, comment_body: CommentSchema):
     validation_object_id(post_id)
     res = post.update_by_comment(post_id, comment_body.dict())
